128388dd234e9e1d72a43acc0bb1b541f105097f
Library Project <library@project.com>
2026-05-21 22:13:33 +0500

    base_url

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -1,8 +1,6 @@
 ﻿import axios from "axios";
 
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ||
-  "http://localhost:3000/api";
+export const API_BASE_URL = "https://library-api-3cn1.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
